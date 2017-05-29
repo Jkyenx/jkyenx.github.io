22bb1d3cd2be012a53835d9530cf7704608fd4e5
jkyenkungu <jovin.kyenkungu@mail.utoronto.ca>
2017-05-29 13:13:59 -0400

    Updated Mobile nav

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ $(document).ready(function(){
   let tab = "";
   let hometab = "";
   let hometablnk = "";
+  let active = false;
+  let prevright = 0;
+
+
 
   $('#links').click(function(event){
       var tar = event.target;
@@ -9,12 +13,7 @@ $(document).ready(function(){
        //event.preventDefault();
       if(tar.innerHTML === 'Home'){
         $(location).attr('href','index.html');
-        //  $(tab).fadeOut(300, function(){
-        //    home();
-        //    tar.innerHTML = hometab;
-        //    tab = "";
-        //  });
-        //alert(window.location.href);
+
       } else {
 
            switch(tar.id){
@@ -105,7 +104,7 @@ $(document).ready(function(){
   });
 
   function hidecont(callback){
-    if($(window).width() > 420) {
+    if($(window).width() > 736) {
     $('#logo').removeClass('logoanimate').css('opacity','1').fadeTo('slow',0).slideUp(
       function(){
         $('#home').animate({
@@ -114,7 +113,7 @@ $(document).ready(function(){
         }, 800);
         //if($(window).width() < 1400){
         // $('#home').addClass('activated');
-        $('#links').children().animate({paddingRight: '3%'}, 800, function(){
+        $('#links').children().animate({paddingRight: '5%'}, 800, function(){
           //$('#links').css({position: 'fixed', backgroundColor: 'black', zIndex: '1'});
         });
       //} else {
@@ -129,8 +128,9 @@ $(document).ready(function(){
       top: '100%'
     },1, function(){
         $('#logo').removeClass('logoanimate').css('opacity','1').fadeTo(1,0).slideUp(function(){
-          $('#home').fadeTo('slow','1');
-          $('#home').css('position','fixed');
+          // $('#home').fadeTo('slow','1');
+          // $('#home').css('position','fixed');
+          $('#bottomnav').fadeIn('slow');
           callback();
         });
 
@@ -139,20 +139,146 @@ $(document).ready(function(){
   }
   }
 
+  $('#navbut').on('click', function(){
+      if(!active){
+      prevright = $('#bottomnav').css('right');
+      //alert(prevright);
+      $('#bottomnav').animate({borderRadius: '0%', right: '0', width: '100%'});
+      active = true;
+      $('#bottomnav a').css('visibility', 'visible');
+    } else {
+      $('#bottomnav').animate({borderRadius: '50%', right: prevright, width: '17%'},function(){
+        $('#bottomnav a').css('visibility', 'hidden');
+      });
+      active = false;
+    }
+  });
+
+
+  $('#links2').click(function(event){
+    switch(event.target.innerHTML){
+      case 'About':
+        if(tab === "") {
+          hidecont(function(){
+         $('#aboutpage').fadeIn(1000);
+         $('#bottomnav').animate({borderRadius: '50%', right: prevright, width: '17%'},function(){
+           $('#bottomnav a').css('visibility', 'hidden');
+         });
+         document.getElementById("About").innerHTML = 'Home';
+         hometab = "About";
+          });
+        } else if(tab !== '#aboutpage') {
+            $(tab).fadeOut(500);
+            $('#bottomnav').animate({borderRadius: '50%', right: prevright, width: '17%'},function(){
+              $('#bottomnav a').css('visibility', 'hidden');
+              $('#aboutpage').fadeIn();
+            });
+            document.getElementById("About").innerHTML = 'Home';
+            hometab = "About";
+        }
+        tab = '#aboutpage';
+        break;
+      case 'Projects':
+        if(tab === "") {
+          hidecont(function(){
+         $('#projectpage').fadeIn(1000);
+         $('#bottomnav').animate({borderRadius: '50%', right: prevright, width: '17%'},function(){
+           $('#bottomnav a').css('visibility', 'hidden');
+         });
+         document.getElementById("Projects").innerHTML = 'Home';
+         hometab = "Projects";
+          });
+        } else if(tab !== '#aboutpage') {
+            $(tab).fadeOut(500);
+            $('#bottomnav').animate({borderRadius: '50%', right: prevright, width: '17%'},function(){
+              $('#bottomnav a').css('visibility', 'hidden');
+              $('#projectpage').fadeIn();
+            });
+            document.getElementById("Projects").innerHTML = 'Home';
+            hometab = "Projects";
+        }
+        tab = '#projectpage';
+        break;
+      case 'Resume':
+        if(tab === "") {
+          hidecont(function(){
+         $('#resumepage').fadeIn(1000);
+         $('#bottomnav').animate({borderRadius: '50%', right: prevright, width: '17%'},function(){
+           $('#bottomnav a').css('visibility', 'hidden');
+         });
+         document.getElementById("Resume").innerHTML = 'Home';
+         hometab = "Resume";
+          });
+        } else if(tab !== '#resumepage') {
+            $(tab).fadeOut(500);
+            $('#bottomnav').animate({borderRadius: '50%', right: prevright, width: '17%'},function(){
+              $('#bottomnav a').css('visibility', 'hidden');
+              $('#resumepage').fadeIn();
+            });
+            document.getElementById("Resume").innerHTML = 'Home';
+            hometab = "Resume";
+        }
+        tab = '#resumepage';
+        break;
+      case 'Contact':
+        if(tab === "") {
+          hidecont(function(){
+         $('#contactpage').fadeIn(1000);
+         $('#bottomnav').animate({borderRadius: '50%', right: prevright, width: '17%'},function(){
+           $('#bottomnav a').css('visibility', 'hidden');
+         });
+         document.getElementById("Contact").innerHTML = 'Home';
+         hometab = "Contact";
+          });
+        } else if(tab !== '#contactpage') {
+            $(tab).fadeOut(500);
+            $('#bottomnav').animate({borderRadius: '50%', right: prevright, width: '17%'},function(){
+              $('#bottomnav a').css('visibility', 'hidden');
+              $('#contactpage').fadeIn();
+            });
+            document.getElementById("Contact").innerHTML = 'Home';
+            hometab = "Contact";
+        }
+        tab = '#contactpage';
+        break;
+      }
+  });
+
+
   function home(callback){
       $('#links').children().animate({paddingRight: '-30px', right: '30px'}, 1300);
       $('#home').animate({
         top: '50%',
         left: '50%'
       }, 1000, function(){
-
         $('#logo').css('opacity','1').fadeIn(1500);
         callback();
       });
 
   }
 
+
+  $(window).on('resize', function() {
+       windowWidth = $(this).width();
+        if (windowWidth < 740) {
+            $("#links").css('display','none');
+            $("#bottomnav").css('display','inherit');
+        } else {
+            $("#links").removeClass('linksanimation').css({display: 'inherit', opacity: "1"});
+            $("#bottomnav").css('display','none');
+        }
+    });
+
+    // window.onorientationchange = function() {
+    //   $('#logo').removeClass('logoanimate').css('opacity','1');
+    //   window.location.reload();
+    // };
+
 });
+
+
+
+// window.onorientationchange = function() { window.location.reload(); };
 
 //animate({ height: 'toggle', opacity: 'toggle' }, 'slow'
 // jQuery.fn.fadeThenSlideToggle = function(speed, easing, callback) {
